ef9ca2d3691a38bb1e9ab7264800efdb15b23f4a
NSaiNagendra <81842328+NSaiNagendra@users.noreply.github.com>
2023-05-29 04:50:23 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,2 +1,2 @@
-variable "ACCESS_KEY" {default = "${{secrets.ACCESS_KEY}}"}
-variable "SECRET_KEY" {default = "${{secrets.SECRET_KEY}}"}
+variable "ACCESS_KEY" {default = "AKIASER33NRWDKK7BFMM"}
+variable "SECRET_KEY" {default = ""}
