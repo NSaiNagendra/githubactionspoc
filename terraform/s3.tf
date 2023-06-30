8271589172905f55cda545c07f2c52301fb262d5
NSaiNagendra <81842328+NSaiNagendra@users.noreply.github.com>
2023-06-30 07:18:53 -0400

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,8 @@
-#resource "aws_s3_bucket" "rafiki_s3" {
-#  bucket = "bidevopsbucket24052023"
-#}
+resource "aws_s3_bucket" "rafiki_s3" {
+  bucket = "bidevopsbucket24052023"
+}
+
+
+resource "aws_s3_bucket" "rafiki_s3" {
+  bucket = "bidevopsbucket240520231"
+}
